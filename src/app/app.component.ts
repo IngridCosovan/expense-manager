7d8915d1233b0008551255a68b01127b92b30c38
Ingrid Cosovan <ingrid.cosovan@gmail.com>
2024-05-11 11:49:48 +0300

    first changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { LoginComponent } from './login/login/login.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DropdownModule,FormsModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'expense-project';
+
 }
---
+++ src/app/login/login/login.component.ts
@@ -0,0 +1,13 @@
+import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  value: string = ''
+}
