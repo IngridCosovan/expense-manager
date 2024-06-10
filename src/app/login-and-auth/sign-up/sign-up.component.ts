5b15fae969b3f135fee56542dd2f1cb037f6e31c
Ingrid Cosovan <ingrid.cosovan@ro.bosch.com>
2024-06-10 18:36:06 +0300

    wip sign-up ui, validators added

--- src/app/login-and-auth/sign-up/sign-up.component.ts
+++ src/app/login-and-auth/sign-up/sign-up.component.ts
@@ -3,8 +3,8 @@ import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} fr
 import {InputTextModule} from "primeng/inputtext";
 import {PasswordModule} from "primeng/password";
 import {
-  PasswordValidator,
-} from "../../shared/password.validator";
+  PasswordValidatorService,
+} from "../../shared/password-validator.service";
 import {CommonModule} from "@angular/common";
 
 @Component({
@@ -17,7 +17,7 @@ import {CommonModule} from "@angular/common";
 export class SignUpComponent implements OnInit{
   signUpForm: FormGroup;
 
-  constructor(private passwordValidator: PasswordValidator) {
+  constructor(private passwordValidator: PasswordValidatorService) {
     this.signUpForm = new FormGroup({
       username: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
